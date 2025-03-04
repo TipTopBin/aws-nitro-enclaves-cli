71e2a4c9e9691bcaffc05ebf4d0904115e65eef9
tiptopbin <270361686@qq.com>
2025-03-04 14:24:38 +0800

    add simple compile files

---
+++ bootstrap/r_env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+if [ -z ${NITRO_CLI_INSTALL_DIR} ];
+then
+    echo "INSTALLDIR variable not set, please set the variable to the location where nitro-cli is installed"
+    return -1
+fi
+
+# lsmod | grep -q nitro_enclaves || \
+#     sudo insmod ${NITRO_CLI_INSTALL_DIR}/lib/modules/$(uname -r)/extra/nitro_enclaves/nitro_enclaves.ko
+
+export PATH=${PATH}:${NITRO_CLI_INSTALL_DIR}/usr/bin/:${NITRO_CLI_INSTALL_DIR}/etc/profile.d/
+export NITRO_CLI_BLOBS=${NITRO_CLI_INSTALL_DIR}/usr/share/nitro_enclaves/blobs
